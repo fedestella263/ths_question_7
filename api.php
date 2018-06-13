500b415765c960403ef47e29db20614379dcc0be
Federico Stella <support@iogame.io>
2018-06-13 00:51:01 +0000

    New validations with error message added

--- api.php
+++ api.php
@@ -1,13 +1,23 @@
 <?php
-    $method = $_SERVER['REQUEST_METHOD'];
-    if($method == 'POST') {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
         header('Content-type: application/json');
         $request = json_decode(file_get_contents('php://input'), true);
-        $response = array("Status" => "OK", "Result" => "");
+        $response = array("Status" => "OK");
         
-        if(!array_key_exists("values", $request) || !array_key_exists("operation", $request) || !array_key_exists(0, $request["values"]) || !array_key_exists(1, $request["values"])) {
+        if(!array_key_exists("values", $request) || !array_key_exists("operation", $request) || 
+            !array_key_exists(0, $request["values"]) || !array_key_exists(1, $request["values"]) ||
+            !is_numeric($request["values"][0]) || !is_numeric($request["values"][1])) {
+            
             $response["Status"] = "error";
+            $response["Message"] = "invalid request";
+            
+        } else if($request["operation"] == "division" && $request["values"][1] == 0) {
+            
+            $response["Status"] = "error";
+            $response["Message"] = "can't be divided by zero";
+            
         } else {
+            
             switch($request["operation"]) {
                 case "sum": 
                     $response["Result"] = $request["values"][0]+$request["values"][1];
@@ -27,6 +37,7 @@
                 
                 default:
                     $response["Status"] = "error";
+                    $response["Message"] = "unrecognized operation";
             }
         }
         
